14247b65d7b2ee06cf1b966674117f59f627af8f
Ivanwagu1 <115166614+Ivanwagu1@users.noreply.github.com>
2022-11-09 07:46:33 +0700

    Update info-tagnobot.js

--- plugins/info-tagnobot.js
+++ plugins/info-tagnobot.js
@@ -2,11 +2,11 @@ let fs = require('fs')
 let fetch = require('node-fetch')
 let handler = async (m, { conn, text }) => {
 let name = m.fromMe ? conn.user : conn.contacts[m.sender]
-let krizyn = 'https://telegra.ph/file/65289acb075702f9bb306.jpg'
+let krizyn = 'https://telegra.ph/file/3eed967909e37d97d6a47.jpg'
 await conn.send3ButtonImg(m.chat, krizyn, "=====『 TAG TERDETEKSI 』=====\n\nIya ada perlu apa dengan bot? ", '📮 Silahkan Pilih Button dibawah ini', 'Group', '.allgc', 'Menu', '.menu', 'Store', '.store', m)
                         
 }
-handler.customPrefix = /(@+6289526416801)/i
+handler.customPrefix = /(@+62819095411460)/i
 handler.command = new RegExp
 
 module.exports = handler
